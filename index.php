4fb669c3b3578e2e8b3ca5d7698443b4f0b7bda8
piiair <pierre.bonnal@hotmail.fr>
2023-02-10 14:22:11 +0100

    conflit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once ROOT . 'views/includes/header.php';
 <body>
     <?php include_once ROOT . 'views/includes/navbar.php'; ?> 
     <h1>Code de pierre</h1>
-        
+     <p>c'est moi le boss</p>
     <?php include_once ROOT . 'views/includes/footer.php'; ?>
 </body>
 
